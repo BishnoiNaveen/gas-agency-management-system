960bff5ccc84660f5038f761a4cf470e5a64f5c0
BishnoiNaveen <BishnoiNaveen@users.noreply.github.com>
2026-09-09 07:50:31 +0000

    deploy: 100b5cb8000a9834434bd825ecb6c48550df33e5

--- sw.js
+++ sw.js
@@ -1,39 +1,13 @@
-const CACHE = 'gams-v6';
-const ASSETS = [
-  './',
-  './index.html',
-  './styles.css',
-  './js/config.js?v=6',
-  './js/api.js?v=6',
-  './js/utils.js?v=6',
-  './app.js?v=6',
-  './manifest.json',
-  './assets/vendor/fontawesome/css/all.min.css',
-  './assets/vendor/dm-sans/400.css',
-  './assets/vendor/dm-sans/600.css',
-  './assets/vendor/dm-sans/700.css',
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
+self.addEventListener('install', () => self.skipWaiting());
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (e) => {
-  if (e.request.method !== 'GET') return;
-  if (e.request.url.includes('/api/')) return;
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, copy));
-      return res;
-    }).catch(() => cached))
-  );
+  e.respondWith(fetch(e.request));
 });
